262bebdd5a4df20c28a69fd6b48638e76d0c1fd9
thine <thinhcubi27@gmail.com>
2021-04-08 16:05:05 +0700

    JS1.js

---
+++ JS1.js
@@ -0,0 +1,16 @@
+let a =+prompt("Nhap a");
+switch (a) {
+    case 0:
+        alert("0");
+        break;
+    case 1:
+        alert("1");
+        break;
+    case 2:
+        alert("2");
+        break;
+    case 3:
+        alert("3");
+    default:
+        alert('6');
+}
